0400d77286366cc9dd6c8696009ea772f005e97f
Alex Poon <chpoon92@gmail.com>
2014-10-22 14:18:41 +0800

    Add doc info

---
+++ src/main/java/hk/ust/comp4321/database/DocInfo.java
@@ -0,0 +1,50 @@
+package hk.ust.comp4321.database;
+
+/**
+ * 
+ * @author Alex Poon
+ */
+public class DocInfo
+{
+
+  //Constants and fields.
+  // -------------------------------------------------------------------------
+
+  private String word;
+  private int frequency;
+
+  // Constructors.
+  // -------------------------------------------------------------------------
+
+  /**
+   * This constrcutor encapsulates the word id and frequency in a document.
+   * 
+   * @param id      the word id to be inserted
+   * @param frequency the associated frequency
+   */
+  public DocInfo(int id, int frequency)
+  {
+    this.id = id;
+    this.frequency = frequency;
+  }
+
+  /**
+   * This method returns the word id of the word instance.
+   * 
+   * @return the word id of the word instance 
+   */
+  public int getId()
+  {
+    return this.word;
+  }
+
+  /**
+   * This method returns the frequency of the word instance.
+   * 
+   * @return the frequency of the word instance 
+   */
+  public int getFrequency()
+  {
+    return this.frequency;
+  }
+}
