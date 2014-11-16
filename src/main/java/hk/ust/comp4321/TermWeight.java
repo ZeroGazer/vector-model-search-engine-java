0c0222ba4c67c7f9c77e0376c64c96c71a4e9562
sywongad <sywongad@stu.ust.hk>
2014-11-16 23:33:30 +0800

    Create TermWeight.java
    Find the termweight of a word

---
+++ src/main/java/hk/ust/comp4321/TermWeight.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.util.List;
+
+import main.java.hk.ust.comp4321.database.DocInfo;
+import main.java.hk.ust.comp4321.database.ForwardIndexTable;
+import main.java.hk.ust.comp4321.database.IndexInfo;
+import main.java.hk.ust.comp4321.database.InvertedIndexTable;
+
+/**
+ * 
+ * @author Paul Wong
+ *
+ */
+
+public class TermWeight {
+	
+	// Constants and fields.
+	// -------------------------------------------------------------------------	
+	
+	InvertedIndexTable invertedIndexTable;
+	private int wordId;
+	private int pageId;
+	private final int totalPage = 300;
+	
+	// Constructors.
+	// -------------------------------------------------------------------------
+
+	/**
+	 * This constructor encapsulates the WordId and PageId.
+	 * 
+	 * @param WordId        the word's id
+	 * @param PageId        the page's id
+	 */
+	public TermWeight(int WordId, int PageId)
+	{
+		wordId = WordId;
+		pageId = PageId;
+	}
+	
+	/**
+	 * This method converts base 10 log to base 2 log.
+	 * 
+	 * @param n        number needed to be log
+	 */
+	private static double log2(int n)
+	{
+	    return (Math.log(n) / Math.log(2));
+	}
+	
+	/**
+	 * This method finds the termWeight by (tf/max. tf) * log (N/df).
+	 * 
+	 * @return termWeight
+	 */
+	public double getTermWeight() throws IOException
+	{
+		int tfmax=0;
+		int tf = invertedIndexTable.getIndexInfo(wordId, pageId).getPositionList().size(); //tf
+		int numOfPage = invertedIndexTable.getIndexInfoList(wordId).size(); //N
+		for(int i=0; i<numOfPage; i++) // max. tf
+		{
+			if (invertedIndexTable.getIndexInfoList(wordId).get(i).getPositionList().size() > tfmax)
+			{
+				tfmax = invertedIndexTable.getIndexInfoList(wordId).get(i).getPositionList().size();
+			}
+		}
+		return tf*tfmax/log2(numOfPage/totalPage);
+	}
+}
