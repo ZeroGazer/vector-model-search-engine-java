e891a96d8c721f743a46640295dc3b91fda35b9c
Alex Poon <chpoon92@gmail.com>
2014-11-20 21:14:58 +0800

    Add test program for page rank

---
+++ src/main/java/hk/ust/comp4321/test/TestPageRank.java
@@ -0,0 +1,13 @@
+package hk.ust.comp4321.test;
+
+import java.io.IOException;
+
+import hk.ust.comp4321.engine.PageRank;
+
+public class TestPageRank
+{
+  public static void main(String[] args) throws IOException
+  {
+    PageRank pageRank = new PageRank("Movie");
+  }
+}
